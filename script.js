4f0c1ed3d2917679c333e48d358d0637491e6095
jamilya-dev <omg.groups@yandex.ru>
2024-07-23 22:38:32 +0300

    Выполнено задание 3

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+let lang = 'ru';
+if (lang === 'ru') {
+  console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+} else if (lang === 'en') {
+  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+}
+switch (true) {
+  case (lang === 'ru'):
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+    break
+  case (lang === 'en'):
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+    break
+}
+let arr = [
+  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+];
+lang === 'ru' ? console.log(arr[1]) : console.log(arr[0])
+
+namePerson = 'Александр';
+namePerson == 'Артем' ? console.log('директор') : namePerson == 'Александр' ? console.log('преподаватель') : console.log('студент');
+
+
 
