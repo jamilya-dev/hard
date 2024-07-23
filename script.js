80531dbdee14fb8cba89f06d3eeb868b64775381
jamilya-dev <omg.groups@yandex.ru>
2024-07-23 10:42:59 +0300

    clear script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-let num = 266219;
-// let numStr = num.toString();
-let arr = num.toString().split("");
-let res = 1;
-for (let i = 0; i < arr.length; i++) {
-  res *= arr[i];
-}
-console.log(res);
-res = (res ** 3);
-let resStr = res.toString().substring(0, 2);
-console.log(resStr);
+
