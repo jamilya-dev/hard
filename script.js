a8325783ec9da7f3d2d623c3178cbee0a233f921
jamilya-dev <omg.groups@yandex.ru>
2024-07-24 21:21:38 +0300

    Работа над ошибками

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let lang = 'ru';
-if (lang === 'ru') {
-  console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-} else if (lang === 'en') {
-  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-}
-switch (true) {
-  case (lang === 'ru'):
-    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-    break
-  case (lang === 'en'):
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-    break
-}
 let arr = [
   ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
   ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
 ];
+let lang = 'ru';
+if (lang === 'ru') {
+  console.log(arr[1]);
+} else if (lang === 'en') {
+  console.log(arr[0]);
+}
+switch (true) {
+  case (lang === 'ru'):
+    console.log(arr[1]);
+    break
+  case (lang === 'en'):
+    console.log(arr[0]);
+    break
+}
 lang === 'ru' ? console.log(arr[1]) : console.log(arr[0])
 
 let namePerson = 'Александр';
