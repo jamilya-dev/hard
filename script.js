11fe92a0bf78ff7119c2c239c196ff4f367ccb78
jamilya-dev <omg.groups@yandex.ru>
2024-07-25 22:53:57 +0300

    выполнено 4 задание

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+'use strict';
 
+function showStr(string) {
+  if ((typeof string) !== 'string') {
+    return 'Это не стока!';
+  } else if ((typeof string) === 'string') {
+    string = string.trim();
+    if (string.length > 30) {
+      string = string.substring(0, 30) + '...';
+    }
+    return string;
+  }
+}
+
+showStr('  В полученной (как аргумент) строке функция должна убрать все пробелы в начале и в конце  ');
