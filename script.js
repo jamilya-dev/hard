775a6bc6db006e5e009d436c1b5ba0f7cb2a5e8d
jamilya-dev <omg.groups@yandex.ru>
2024-07-31 10:57:23 +0300

    Выполнено задание 9

--- script.js
+++ script.js
@@ -1,104 +1,28 @@
 'use strict';
 
-const appData = {
-  title: '',
-  screens: [],
-  screenPrice: 0,
-  adaptive: true,
-  rollback: 10,
-  allServicePrices: 0,
-  fullPrice: 0,
-  servicePercentPrice: 0,
-  services: {},
-  start: function () {
-    appData.asking();
-    appData.addPrices();
-    appData.getFullPrice();
-    appData.getServicePercentPrices();
-    appData.getTitle();
-    appData.logger();
-  },
-  isNumber: function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-  },
-  isString: function (str) {
-    return isNaN(str);
-  },
-  asking: function () {
-    do {
-      appData.title = prompt('Как называется ваш проект?');
-    } while (!appData.isString(appData.title));
+const monts = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
+const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+const hours = ["час", "часа", "часов"];
 
-    for (let i = 0; i < 2; i++) {
-      let price = 0
-      let name = ''
-      do {
-        name = prompt('Какие типы экранов нужно разработать?');
-      } while (!appData.isString(name));
+let dateOne = document.createElement('div');
+let dateTwo = document.createElement('div');
 
-      do {
-        price = prompt('Сколько будет стоить данная работа?');
-      } while (!appData.isNumber(price))
-
-      appData.screens.push({ id: i, name: name, price: price })
-    }
-
-    for (let i = 0; i < 2; i++) {
-      let price = 0
-      let name = ''
-      do {
-        name = prompt('Какой дополнительный тип услуги нужен?');
-      } while (!appData.isString(name));
-
-      do {
-        price = prompt('Сколько это будет стоить?');
-      } while (!appData.isNumber(price))
-
-      appData.services[name + '-' + i] = +price
-    }
-
-    appData.adaptive = confirm("Нужен ли адаптив на сайте?")
-  },
-  addPrices: function () {
-    appData.screenPrice = appData.screens.reduce(function (sum, screen) {
-      return sum + +screen.price;
-    }, 0)
-
-    for (let key in appData.services) {
-      appData.allServicePrices += appData.services[key]
-    }
-  },
-  getFullPrice: function getFullPrice() {
-    appData.fullPrice = +appData.screenPrice + appData.allServicePrices;;
-  },
-  getServicePercentPrices: function getServicePercentPrices() {
-    appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
-  },
-  getTitle: function getTitle() {
-    appData.title = appData.title.trim()[0].toUpperCase() + appData.title.trim().substring(1).toLocaleLowerCase();
-  },
-  getRollbackMessage: function (price) {
-    switch (true) {
-      case price >= 30000:
-        return "Даем скидку в 10%";
-        break
-      case price >= 15000 && price < 30000:
-        return 'Даем скидку в 5%';
-        break
-      case price >= 0 && price < 15000:
-        return 'Скидка не предусмотрена';
-        break
-      case price < 0:
-        return 'Что то пошло не так';
-        break
-    }
-  },
-  logger: function () {
-    for (let key in appData) {
-      console.log(key + ' ' + appData[key]);
-    }
-    console.log(appData.services)
+const numWord = function (hour) {
+  if (hour === 1) {
+    return hours[0];
+  } else if (hour > 1 && hour < 5) {
+    return hours[1];
+  } else {
+    return hours[2];
   }
 }
 
-appData.start();
+const showTime = setInterval(function () {
+  const date = new Date();
+  const shortDate = date.toLocaleString("ru")
+  dateOne.textContent = 'Сегодня ' + days[date.getDay()] + ' ' + date.getDate() + ', ' + monts[date.getMonth()] + ' ' + date.getFullYear() + ' года, ' + date.getHours() + ' ' + numWord(date.getHours()) + ' ' + date.getMinutes() + ' минут ' + date.getSeconds() + ' секунды';
+  dateTwo.textContent = shortDate;
+}, 1000)
+
+document.body.append(dateOne);
+document.body.append(dateTwo);
