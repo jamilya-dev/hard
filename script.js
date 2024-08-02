ec2800469813871eb6c548d2494ead682dfa5918
jamilya-dev <omg.groups@yandex.ru>
2024-08-02 13:41:58 +0300

    доработка задания 9

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ div.innerHTML = "";
 
 for (let key in week) {
   if (week[key] === 'суббота' || week[key] === 'воскресенье') {
-    div.innerHTML += '<i>' + week[key] + '</i>' + '</br>';
+    div.innerHTML += `<i>${week[key]}</i></br>`;
   } else if (key == (date.getDay() - 1)) {
-    div.innerHTML += '<b>' + week[key] + '</b>' + '</br>';
+    div.innerHTML += `<b>${week[key]}</b></br>`;
   } else {
-    div.innerHTML += '<font>' + week[key] + '</font>' + '</br>';
+    div.innerHTML += `<font>${week[key]}</font></br>`;
   }
   document.body.append(div);
 }
